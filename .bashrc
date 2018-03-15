6441e6bbccc898661ec86929f26adde115980ea7
jahnava <jahnavalmt@gmail.com>
2018-03-15 18:58:36 -0400

    initial commit

---
+++ .bashrc
@@ -0,0 +1,1 @@
+alias yarn='node "C:\Users\Jahnava Dev Academy\AppData\Roaming\npm\node_modules\yarn\bin\yarn.js
--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class App extends React.Component {
   render() {
-    return <p>Our starting point ...</p>;
+    return <p>Our starting point ... Does this change live?</p>;
   }
 }
