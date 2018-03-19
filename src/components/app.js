a27024f0f9334fdaba19cff0e30e78390cc1b04c
jahnava <jahnavalmt@gmail.com>
2018-03-18 22:40:39 -0400

    added footer ourline

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 export default class App extends React.Component {
   render() {
-    return <p>Our starting point ... Does this change live?</p>;
+    return (
+
+    <div>
+        <p>Our starting point ...</p>
+     </div>
+
+     );
   }
 }
