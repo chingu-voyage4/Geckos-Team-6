2a93b4c4753beab3bc3d8ce8395fd61012d8c803
JHart9 <jessicahart.dev@gmail.com>
2018-03-22 09:09:48 -0500

    Fixed package files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
-import Footer from "./components/footer/footer";
-
-ReactDOM.render(<App />, document.getElementById("app"));
-ReactDOM.render(<Footer />, document.getElementById("footer"));
